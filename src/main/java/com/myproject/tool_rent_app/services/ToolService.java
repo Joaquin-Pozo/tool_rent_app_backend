e50723889cb5c3f700a2768aa57b110317887669
Joaquín Pozo <joaquin.pozo@usach.cl>
2025-12-09 22:12:16 -0300

    New validations before add a new tool.

--- src/main/java/com/myproject/tool_rent_app/services/ToolService.java
+++ src/main/java/com/myproject/tool_rent_app/services/ToolService.java
@@ -11,6 +11,7 @@ import com.myproject.tool_rent_app.repositories.ToolStateRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
@@ -44,6 +45,25 @@ public class ToolService {
     // RF 1.1: Registrar nuevas herramientas con datos básicos (nombre, categoría, estado
     // inicial, valor de reposición) -> Actualizar el kardex
     public ToolEntity createTool(ToolEntity tool){
+        if (tool.getName() == null || tool.getName().isBlank()) {
+            throw new RuntimeException("Debe ingresar un nombre para registrar la herramienta");
+        }
+        if (tool.getToolIdentifier() == null || tool.getToolIdentifier().isBlank()) {
+            throw new RuntimeException("Debe ingresar un identificador (SKU) para registrar la herramienta");
+        }
+        if (tool.getCategory() == null || tool.getCategory().isBlank()) {
+            throw new RuntimeException("Debe ingresar un categoría para registrar la herramienta");
+        }
+        if (tool.getReplacementCost() == null || tool.getReplacementCost().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new RuntimeException("Debe ingresar un costo de reposición válido y mayor a 0");
+        }
+        if (tool.getPrice() == null || tool.getPrice().compareTo(BigDecimal.ZERO) <= 0) {
+            throw new RuntimeException("Debe ingresar un precio válido y mayor a 0");
+        }
+        if (tool.getStock() < 0) {
+            throw new RuntimeException("Debe ingresar un stock mayor igual a 0");
+        }
+
         // Agrega la nueva herramienta
         ToolEntity savedTool = toolRepository.save(tool);
 
